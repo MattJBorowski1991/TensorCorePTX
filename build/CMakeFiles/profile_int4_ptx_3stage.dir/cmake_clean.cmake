f2a6c9141c9a7a3d0b372bbccb5b2a7b15c1463b
Matt J. Borowski <mattjborowski@gmail.com>
2026-05-08 17:34:17 +0000

    int4: add the triple buffered kernel

---
+++ build/CMakeFiles/profile_int4_ptx_3stage.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/profile_int4_ptx_3stage.dir/kernels/int4_ptx_3stage.cu.o"
+  "CMakeFiles/profile_int4_ptx_3stage.dir/kernels/int4_ptx_3stage.cu.o.d"
+  "CMakeFiles/profile_int4_ptx_3stage.dir/src/data_int4.cu.o"
+  "CMakeFiles/profile_int4_ptx_3stage.dir/src/data_int4.cu.o.d"
+  "CMakeFiles/profile_int4_ptx_3stage.dir/src/main_int4.cu.o"
+  "CMakeFiles/profile_int4_ptx_3stage.dir/src/main_int4.cu.o.d"
+  "CMakeFiles/profile_int4_ptx_3stage.dir/src/solver_int4.cu.o"
+  "CMakeFiles/profile_int4_ptx_3stage.dir/src/solver_int4.cu.o.d"
+  "profile_int4_ptx_3stage"
+  "profile_int4_ptx_3stage.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/profile_int4_ptx_3stage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
